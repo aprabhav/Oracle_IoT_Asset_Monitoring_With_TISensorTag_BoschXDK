c9712425020321ea063bb1b1af83c3af8878d9c7
aprabhav <abhimanyuprabhavalkar@gmail.com>
2018-05-06 17:23:27 +0530

    Update BleGattCallback.java

--- src/main/java/com/oracle/iot/sample/tisensortag/Bluetooth/BleGattCallback.java
+++ src/main/java/com/oracle/iot/sample/tisensortag/Bluetooth/BleGattCallback.java
@@ -1,33 +1,11 @@
-package com.oracle.iot.sample.tisensortag;
+package com.oracle.iot.sample.tisensortag.Bluetooth;
 
 
-import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
+
+import com.oracle.iot.sample.tisensortag.Device.SensorDevice;
 
 public abstract class BleGattCallback extends BluetoothGattCallback{
 
-    abstract void setSensorDevice (SensorDevice device);
-
-    @Override
-    abstract public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState);
-
-    @Override
-    abstract public void onServicesDiscovered(BluetoothGatt gatt, int status);
-
-    @Override
-    abstract public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status);
-
-    @Override
-    abstract public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status);
-
-    @Override
-    abstract public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic);
-
-    @Override
-    abstract public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status);
-
-    @Override
-    abstract public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status);
+    public abstract void setSensorDevice (SensorDevice device);
 }
